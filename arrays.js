b73dc533c1ef279487ae91a82d70e5acfb01d37e
panosrodo <brandonroots@hotmail.gr>
2025-12-01 18:05:23 +0200

    fix: add comment

--- arrays.js
+++ arrays.js
@@ -1,14 +1,16 @@
-// const arr1 = []
+const arr1 = []
 const arr2 = [1, 2, 3, 4, 5]
-// const arr3 = new Array(5).fill(0)       // [0, 0, 0, 0, 0] otherwise [undefined, undefined, undefined, undefined, undefined]
+const arr3 = new Array(5).fill(0)       // [0, 0, 0, 0, 0] otherwise [undefined, undefined, undefined, undefined, undefined]
 
-// const arr = [...arr1, arr2]                       // spread operator gives a shallow copy
+const arr = [...arr1, arr2]                       // spread operator gives a shallow copy
 
-// //Array methods
-// console.log(arr2.length)
-// console.log(arr2[0])
-// console.log(arr2[arr2.length - 1])
-// console.log(arr2.join(','))         // επιστρέφει string
+//Array methods
+console.log(arr2.length)
+console.log(arr2[0])
+console.log(arr2[arr2.length - 1])
+
+// Join
+console.log(arr2.join(','))         // επιστρέφει string
 
 // Traverse
 for (let i = 0; i < arr2.length; i++) {
