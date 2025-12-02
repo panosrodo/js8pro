c355a1fa42d924f7af1f5761cf9119f03325a28c
panosrodo <brandonroots@hotmail.gr>
2025-12-02 07:30:49 +0200

    feat: add number exercises

--- number-exercises.js
+++ number-exercises.js
@@ -1,5 +1,4 @@
 // Currency Formatter
-
 const formatCurrency = amount => {
     if (!Number.isFinite(amount)) return "Invalid Amount";
     return `${amount.toFixed(2)}`
@@ -7,4 +6,28 @@ const formatCurrency = amount => {
 
 console.log(formatCurrency(12.3456))        // 12.34
 console.log(formatCurrency(NaN))
+
+// Safe Division
+const safeDivide = (a, b) => {
+    a = a ?? 0;     // nullish coalescing
+    b = b ?? 1;
+
+    return (b === 0) ? Infinity : a / b;
+}
+
+console.log(safeDivide(10, 2))
+console.log(safeDivide(10, 0))
+console.log(safeDivide(null, 2))
+
+// Random Hex Color Generator
+const getRandomHexColor = () => {
+    const color = Math.floor(Math.random() * (0xFFFFFF + 1)).toString(16).padStart(6, '0')
+    return `#${color}`
+}
+
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
 
