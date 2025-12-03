85e1dd37ba38a07319a8ba7f6b60461d1cebe0d0
panosrodo <brandonroots@hotmail.gr>
2025-12-03 09:11:11 +0200

    feat: clear

--- objects.js
+++ objects.js
@@ -3,6 +3,12 @@ point3D.x = 5;
 point3D.y = 10;
 point3D.z = 20;
 
+const user = {
+    firstname: 'Alice',
+    lastname: 'Wonderland',
+    hobbies: [ {id: 1, title: "Hobbi1"}, {id: 2, title: "tennis"} ]
+}
+
 const point2D = {
     x: 10,
     y: 20,
@@ -40,3 +46,29 @@ const cart = {
 cart.addItem('Milk', 2)
 cart.addItem('Eggs', 5)
 console.log(cart.calculateTotal())  // 7
+
+// Object destructuring
+const { x: myX, y, z = 0, t = 0 } = point3D
+console.log(`${myX}, ${y}, ${z}`)
+
+// without distructuring - verbose
+function display3DPoint(point) {
+    console.log(`x: ${point['x']}`)
+    console.log(`y: ${point['y']}`)
+    console.log(`z: ${point['z']}`)
+}
+
+// with destructuring - not verbose
+function display3DPoint2({ x, y, z = 'Not provided' }) {
+    console.log(`x: ${x}`)
+    console.log(`y: ${y}`)
+    console.log(`z: ${z}`)
+}
+
+const p2 = {
+    x: 15,
+    y: 18,
+    z: 22
+}
+
+display3DPoint2(p2)
