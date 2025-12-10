7c993838e08c8aa0f49efdac8f5f66776968c386
panosrodo <brandonroots@hotmail.gr>
2025-12-10 14:38:07 +0200

    feat: add model and view methods

--- browser/counterapp/main.js
+++ browser/counterapp/main.js
@@ -10,6 +10,45 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 // Controllers
+function onDecreaseClicked() {
+    decreaseCounter()
+}
+
+function onResetClicked() {
+    resetCounter()
+}
+
+function onIncreaseClicked() {
+    increaseCounter()
+}
 
 
+// Model
+function decreaseCounter() {
+    counter--;
+    render()
+}
 
+function resetCounter() {
+    counter = DEFAULT
+    render()
+}
+
+function increaseCounter() {
+    counter++
+    render()
+}
+
+// View
+function render() {
+    const counterDOM = document.querySelector('#counter')
+    counterDOM.textContent = counter
+    styleCounter(counterDOM)
+}
+
+
+function styleCounter(counterDOM) {
+    counterDOM.classList.toggle('color-green', counter > 0)
+    counterDOM.classList.toggle('color-red', counter < 0)
+    counterDOM.classList.toggle('color-black', counter === 0)
+}
